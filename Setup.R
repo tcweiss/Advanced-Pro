625b4989d469d53be95f678ea49db68d289ce074
Samuel Duerr <samuel.duerr@student.unisg.ch>
2022-05-07 10:20:29 +0200

    test

--- Setup.R
+++ Setup.R
@@ -2,12 +2,12 @@
 library(usethis)
 create_from_github("tcweiss/Advanced-Pro",
                    fork = TRUE,
-                   destdir = "/Users/thomasweiss/Desktop/Uni/Advanced Programming/Code",
+                   destdir = "/Users/duerr/OneDrive/Documents/Ausbildung/HSG/FS22/Programming/Advanced-Pro",
                    protocol = "https")
 
 # In new project, create a new branch with a sensitive name.
 library(usethis)
-pr_init(branch = "thomas")
+pr_init(branch = "samuel")
 
 # Run this to activate the branch.
 pr_push()
